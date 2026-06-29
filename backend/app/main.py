3e468057e67fb1429d5a1ede0d0be36e732256bf
alberthgrande <alberthgrande98@gmail.com>
2026-06-29 17:13:23 +0800

    modified main.py added backend run and added empty inii file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -33,3 +34,10 @@ def customers():
     return {
         "status": "ok"
     }
+
+if __name__ == "__main__": 
+    uvicorn.run( 
+        "app.main:app", 
+        host="127.0.0.1", 
+        port=8000, reload=True, 
+    )
